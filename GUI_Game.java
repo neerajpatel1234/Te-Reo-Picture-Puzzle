7d6383814187dbfa7065af0f39a47aad9c58b449
Neeraj Patel <neerajnpatel18@gmail.com>
2024-02-11 21:37:15 +1300

    - Basic GUI work

--- GUI_Game.java
+++ GUI_Game.java
@@ -1,5 +1,44 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * GUI_Game.java
+ *
+ * This class is the main class for the GUI of the game. It will be responsible for
+ * creating the GUI and handling the user input.
+ */
 public class GUI_Game {
+    // --------------------------- Instance Variables ---------------------------
+    private List<Word> wordsList;
+    private List<Phrase> phrasesList;
+    private List<Colour> coloursList;
+
+
+
+
+    // ------------------------------- Constructor -------------------------------
+    public GUI_Game() {
+        setupGame();
+    }
+
+
+
     public void startGame() {
         throw new UnsupportedOperationException("Not supported yet.");
     }
+
+
+    private void setupGame() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private void drawGrid() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+
+
 }
+
