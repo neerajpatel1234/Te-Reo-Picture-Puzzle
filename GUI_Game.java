1bb281f9b63a97b2dc75a7823f1ab112e6f6ec6a
Neeraj Patel <neerajnpatel18@gmail.com>
2024-02-11 21:42:19 +1300

    - finished set up method

--- GUI_Game.java
+++ GUI_Game.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -13,6 +14,9 @@ public class GUI_Game {
     private List<Word> wordsList;
     private List<Phrase> phrasesList;
     private List<Colour> coloursList;
+    private int score;
+    private int incorrectAnswers;
+
 
 
 
@@ -30,7 +34,53 @@ public class GUI_Game {
 
 
     private void setupGame() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        wordsList = new ArrayList<>();
+        phrasesList = new ArrayList<>();
+        coloursList = new ArrayList<>();
+        score = 0;
+        incorrectAnswers = 0;
+        setupWords();
+        setupPhrases();
+        setupColours();
+    }
+
+    private void setupPhrases() {
+        phrasesList = new ArrayList<>();
+        phrasesList.add(new Phrase("Kia ora", "Hello"));
+        phrasesList.add(new Phrase("Kei te pēhea koe?", "How are you?"));
+        phrasesList.add(new Phrase("Ko wai tō ingoa?", "What is your name?"));
+        phrasesList.add(new Phrase("Kei te pai", "Good"));
+        phrasesList.add(new Phrase("Kei te kino", "Bad"));
+        phrasesList.add(new Phrase("Kei te mahi", "Work"));
+        phrasesList.add(new Phrase("Kei te kai", "Eat"));
+        phrasesList.add(new Phrase("Kei te moe", "Sleep"));
+        phrasesList.add(new Phrase("Kei te haere", "Go"));
+        phrasesList.add(new Phrase("Kei te noho", "Sit"));
+        phrasesList.add(new Phrase("Kei te tū", "Stand"));
+    }
+
+    public void setupWords() {
+        wordsList = new ArrayList<>();
+        wordsList.add(new Word("Tahi", "One"));
+        wordsList.add(new Word("Rua" , "Two"));
+        wordsList.add(new Word("Toru", "Three"));
+        wordsList.add(new Word("Wha", "Four"));
+        wordsList.add(new Word("Rima", "Five"));
+        wordsList.add(new Word("Ono", "Six"));
+        wordsList.add(new Word("Whitu", "Seven"));
+        wordsList.add(new Word("Waru", "Eight"));
+        wordsList.add(new Word("Iwa", "Nine"));
+        wordsList.add(new Word("Tekau", "Ten"));
+    }
+
+    public void setupColours() {
+        coloursList = new ArrayList<>();
+        coloursList.add(new Colour("Mā", "White"));
+        coloursList.add(new Colour("Mangu", "Black"));
+        coloursList.add(new Colour("Whero", "Red"));
+        coloursList.add(new Colour("Karaka", "Orange"));
+        coloursList.add(new Colour("Kākāriki", "Green"));
+        coloursList.add(new Colour("Kikorangi", "Blue"));
     }
 
     private void drawGrid() {
